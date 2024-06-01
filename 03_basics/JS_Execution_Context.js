8f60edd12eaaa2e819515ccf44fc3d4cf1d4ab6e
naveen-pandey <naveenpandey0502@gmail.com>
2024-06-01 20:24:34 +0530

    execution context

---
+++ 03_basics/JS_Execution_Context.js
@@ -0,0 +1,16 @@
+// ********** Javascript Execution Context **********
+
+Javascript runs program in 2 phases
+
+Global EC-  GEC hoga hi hoga. this keyword me Global EC ko rakh diya jata hai. Browser me this ki value Window object ayegi. 
+            Node me alag hoti hai
+
+
+Function EC- 
+
+
+Eval EC- 
+
+
+Call stack-  works on LIFO
+
