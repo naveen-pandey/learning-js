3ecbcd1edbde1546218526125282c96e961a27b9
naveen-pandey <naveenpandey0502@gmail.com>
2024-06-13 15:28:21 +0530

    one and switch

---
+++ 04_control_flow/one.js
@@ -0,0 +1,67 @@
+const arr = ["name","shame"];
+console.log(arr.toString());
+
+const obj = {};
+console.log(obj);
+
+obj.name = "FrontendMaster";
+console.log(obj.name);
+
+obj[arr] = "Reacttt";
+console.log(obj);
+
+console.log(obj.name);
+
+
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
